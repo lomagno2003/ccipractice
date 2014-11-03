d5d52700bbd2ad17d5134dc1f8d55b2f5bfedcf6
lomagno2003 <lomagno2003@gmail.com>
2014-11-03 10:21:27 -0300

    Question4.5

---
+++ DataStructures_ArraysAndStrings_Java/src/Question4_5/Node.java
@@ -0,0 +1,11 @@
+package Question4_5;
+
+public class Node {
+	public Node left = null;
+	public Node right = null;
+	public Integer value = null;
+
+	public Node(Integer value) {
+		this.value = value;
+	}
+}
---
+++ DataStructures_ArraysAndStrings_Java/src/Question4_5/Solution.java
@@ -0,0 +1,62 @@
+package Question4_5;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Solution {
+	 private Integer lastValue;
+	 
+	 public Boolean isBinarySearchTree(Node root){
+	 	lastValue = null;
+	 	
+	 	return dfs(root);
+	 }
+	 
+	 /* O(N) time, and O(N) space */
+	 private Boolean dfs(Node root){
+	 	if(root!=null){
+	 		if(dfs(root.left)){
+	 			if(lastValue==null){
+	 				/* The first element */
+	 				lastValue = root.value;
+	 				
+	 				return true;
+	 			} else {
+		 			if(root.value>lastValue){
+		 				lastValue = root.value;
+		 				
+		 				return dfs(root.right);
+		 			} else {
+		 				/* The tree is not ordered */
+		 				return false;
+		 			}
+		 		}
+	 		} else {
+	 			return false;
+	 		}
+	 	} else {
+	 		/* A empty tree could be a empty search tree */
+	 		return true;
+	 	}
+	 }
+	 
+	 public static void main(String [] args){
+		 Node root = new Node(4);
+		 root.left = new Node(2);
+		 root.left.left = new Node(1);
+		 root.left.right = new Node(3);
+		 root.right = new Node(6);
+		 root.right.left = new Node(5);
+		 root.right.right = new Node(7);
+		 
+		 Boolean result = new Solution().isBinarySearchTree(root);
+		 
+		 if(result) {
+			 System.out.println("Is binary search tree");
+		 } else {
+			 System.out.println("Isn't binary search tree");
+		 }
+	 }
+	 
+}
